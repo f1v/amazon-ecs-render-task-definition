643447b217a1873576c733d3b77d0da4308025e1
Fabio Elia <fabio@f1v.co>
2020-06-25 12:24:02 -0400

    feat(volumes + mounts): Add support for custom volumes + mounts

--- index.js
+++ index.js
@@ -6,28 +6,92 @@ const fs = require('fs');
 async function run() {
   try {
     // Get inputs
-    const taskDefinitionFile = core.getInput('task-definition', { required: true });
+    const taskDefinitionFile = core.getInput('task-definition', {
+      required: true,
+    });
     const containerName = core.getInput('container-name', { required: true });
     const imageURI = core.getInput('image', { required: true });
+    const mountPoints = core.getInput('mount-points', { required: false });
+    const volumes = core.getInput('volumes', { required: false });
 
     // Parse the task definition
-    const taskDefPath = path.isAbsolute(taskDefinitionFile) ?
-      taskDefinitionFile :
-      path.join(process.env.GITHUB_WORKSPACE, taskDefinitionFile);
+    const taskDefPath = path.isAbsolute(taskDefinitionFile)
+      ? taskDefinitionFile
+      : path.join(process.env.GITHUB_WORKSPACE, taskDefinitionFile);
     if (!fs.existsSync(taskDefPath)) {
-      throw new Error(`Task definition file does not exist: ${taskDefinitionFile}`);
+      throw new Error(
+        `Task definition file does not exist: ${taskDefinitionFile}`
+      );
     }
     const taskDefContents = require(taskDefPath);
 
     // Insert the image URI
     if (!Array.isArray(taskDefContents.containerDefinitions)) {
-      throw new Error('Invalid task definition format: containerDefinitions section is not present or is not an array');
+      throw new Error(
+        'Invalid task definition format: containerDefinitions section is not present or is not an array'
+      );
     }
-    const containerDef = taskDefContents.containerDefinitions.find(function(element) {
+    const containerDef = taskDefContents.containerDefinitions.find(function (
+      element
+    ) {
       return element.name == containerName;
     });
     if (!containerDef) {
-      throw new Error('Invalid task definition: Could not find container definition with matching name');
+      throw new Error(
+        'Invalid task definition: Could not find container definition with matching name'
+      );
+    }
+
+    if (volumes) {
+      const volumeList = JSON.parse(volumes);
+      if (!Array.isArray(volumeList)) {
+        throw new Error(
+          'Invalid mount point: Volumes should be specified as an array of objects'
+        );
+      }
+
+      volumeList.forEach(function (volume) {
+        taskDefContents.volumes.push({
+          efsVolumeConfiguration: null,
+          name: volume.name,
+          host: {
+            sourcePath: volume.path,
+          },
+          dockerVolumeConfiguration: null,
+        });
+      });
+      // [{"source-volume-name":"/host/source/path"}]/
+    }
+
+    if (mountPoints) {
+      const mounts = JSON.parse(mountPoints);
+      if (!Array.isArray(mounts)) {
+        throw new Error(
+          'Invalid mount point: Mounts should be specified as an array of objects'
+        );
+      }
+
+      mounts.forEach(function (mountEntry) {
+        const container = taskDefContents.containerDefinitions.find(function (
+          element
+        ) {
+          return element.name == mountEntry.container;
+        });
+        container.mountPoints.push({
+          readOnly: null,
+          containerPath: mountEntry.path,
+          sourceVolume: mountEntry.source,
+        });
+
+        const volume = taskDefContents.volumes.find(function (element) {
+          return element.name == mountEntry.source;
+        });
+        if (!volume) {
+          throw new Error(
+            `Invalid task definition: Could not find volume with name ${mountEntry.source}`
+          );
+        }
+      });
     }
     containerDef.image = imageURI;
 
@@ -37,13 +101,12 @@ async function run() {
       prefix: 'task-definition-',
       postfix: '.json',
       keep: true,
-      discardDescriptor: true
+      discardDescriptor: true,
     });
     const newTaskDefContents = JSON.stringify(taskDefContents, null, 2);
     fs.writeFileSync(updatedTaskDefFile.name, newTaskDefContents);
     core.setOutput('task-definition', updatedTaskDefFile.name);
-  }
-  catch (error) {
+  } catch (error) {
     core.setFailed(error.message);
   }
 }
@@ -52,5 +115,5 @@ module.exports = run;
 
 /* istanbul ignore next */
 if (require.main === module) {
-    run();
+  run();
 }
